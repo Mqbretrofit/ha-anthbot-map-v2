aa4e4f194863ea7b1ee7fb06d6f4b5a1cff5fc61
Mqbretrofit <matrai.attila975@gmail.com>
2026-09-04 10:46:03 +0200

    Add Genie model module

---
+++ custom_components/anthbot_map/models/genie.py
@@ -0,0 +1,9 @@
+"""Genie model boundary.
+
+Genie intentionally uses the untouched 2.4.3-beta.3 coordinator.
+"""
+
+from ..coordinator import AnthbotGenieDataUpdateCoordinator
+
+Coordinator = AnthbotGenieDataUpdateCoordinator
+MODEL_FAMILY = "genie"
