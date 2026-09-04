fed40fcd2802eb246493eff97f7cbd89a4dee17d
Mqbretrofit <matrai.attila975@gmail.com>
2026-09-04 13:31:05 +0200

    Enable M-series mowing target persistence

--- custom_components/anthbot_map/models/m_series_common.py
+++ custom_components/anthbot_map/models/m_series_common.py
@@ -3,12 +3,13 @@
 from .m_series_legacy import install_m_series_compat as _install_legacy
 from .m_series_map import install_m_series_map_support
 from .m_series_path import install_m_series_path_support
+from .m_series_status import install_m_series_status_support
 
 _INSTALLED = False
 
 
 def install_m_series_compat() -> None:
-    """Install rebuild compatibility, then the verified test4 map/path layers."""
+    """Install rebuild compatibility, then the verified test4 M-series layers."""
     global _INSTALLED
     if _INSTALLED:
         return
@@ -16,3 +17,4 @@ def install_m_series_compat() -> None:
     _install_legacy()
     install_m_series_path_support()
     install_m_series_map_support()
+    install_m_series_status_support()
