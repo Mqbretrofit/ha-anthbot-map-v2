41cc91fed89d06a28ccf53bcae14ee16f7b999e8
Mqbretrofit <matrai.attila975@gmail.com>
2026-09-04 08:08:17 +0200

    Add Genie model surface under models

---
+++ custom_components/anthbot_map/models/genie.py
@@ -0,0 +1,7 @@
+"""ANTHBOT Genie model module.
+
+The proven 2.4.3-beta.3 Genie runtime remains unchanged in the parent package;
+this module is the model-facing import surface inside the dedicated models package.
+"""
+
+from ..genie import *  # noqa: F401,F403
