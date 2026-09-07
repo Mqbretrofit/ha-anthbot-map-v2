5e3cdf63460af199800cb1d91e4c7cf93f90a1ab
Mqbretrofit <matrai.attila975@gmail.com>
2026-09-07 22:02:45 +0200

    Run dashboard embedding wrapper

--- server/anthbot_reporting/run.sh
+++ server/anthbot_reporting/run.sh
@@ -5,7 +5,7 @@ export ANTHBOT_DB_PATH="/data/anthbot_reporting.sqlite3"
 export ANTHBOT_ADMIN_TOKEN="$(bashio::config 'admin_token')"
 
 bashio::log.info "Starting ANTHBOT Reporting Server on port 8080"
-exec uvicorn app:app \
+exec uvicorn asgi:app \
   --host 0.0.0.0 \
   --port 8080 \
   --proxy-headers \
